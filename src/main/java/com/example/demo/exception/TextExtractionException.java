8df5a0d15182dd32b9b5f7f20e6a3edf0b5f7dd9
sarthak6168 <sarthakavachat45@gmail.com>
2026-07-29 13:54:26 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/exception/TextExtractionException.java
@@ -0,0 +1,11 @@
+// exception/TextExtractionException.java
+package com.example.demo.exception;
+
+public class TextExtractionException extends RuntimeException {
+    public TextExtractionException(String message) {
+        super(message);
+    }
+    public TextExtractionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
